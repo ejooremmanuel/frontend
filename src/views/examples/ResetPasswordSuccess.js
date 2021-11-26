4a4c3fd4cb21a700cd52d43dc4ade17362c5bfc4
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-11-26 18:44:09 +0100

    all

--- src/views/examples/ResetPasswordSuccess.js
+++ src/views/examples/ResetPasswordSuccess.js
@@ -1,29 +1,31 @@
-import React, {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom';
-import {confirmRegister} from "../../network/ApiAxios";
-import {Card, CardBody, Col} from "reactstrap";
+import React, { useEffect } from "react";
+// import { useParams } from 'react-router-dom';
+// import {confirmRegister} from "../../network/ApiAxios";
+import { Card, CardBody, Col } from "reactstrap";
 
-const ResetPasswordSuccess = props => {
+const ResetPasswordSuccess = (props) => {
+  useEffect(() => {
+    setTimeout(() => {
+      props.history.push("/auth/login");
+    }, 5000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    useEffect(() => {
-        setTimeout(() => {
-            props.history.push("/auth/login");
-        }, 5000);
-    }, [])
-
-    return (
-        <>
-            <Col lg="6" md="8">
-                <Card className="bg-secondary shadow border-0">
-                    <CardBody className="px-lg-5 py-lg-5">
-                        <div className="text-center mb-4">
-                            <h1>Password reset confirmed! You will be redirected to login...</h1>
-                        </div>
-                    </CardBody>
-                </Card>
-            </Col>
-        </>
-    )
+  return (
+    <>
+      <Col lg="6" md="8">
+        <Card className="bg-secondary shadow border-0">
+          <CardBody className="px-lg-5 py-lg-5">
+            <div className="text-center mb-4">
+              <h1>
+                Password reset confirmed! You will be redirected to login...
+              </h1>
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
+    </>
+  );
 };
 
 export default ResetPasswordSuccess;
